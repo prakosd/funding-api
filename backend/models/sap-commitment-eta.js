94deb5cbc41b447f13cf686dff5247246257ef7f
prakosd <prakosd@yahoo.co.id>
2019-11-13 16:19:54 +0700

    next: user ETA on commitment

---
+++ backend/models/sap-commitment-eta.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const sapCommitmentEta = mongoose.Schema({
+    orderNumber: { type: String, required: true },
+    documentNumber: { type: String, required: true },
+    poNumber: { type: String, required: true },
+});
+sapPrToPo.index({ orderNumber: 1, prNumber: 1, poNumber: 1 }, { unique: true });
+sapPrToPo.plugin(uniqueValidator, {
+    message : 'Order number, PR Number & PO Number must be unique.'
+  });
+
+module.exports = mongoose.model("SapPrToPo", sapPrToPo);
+  
